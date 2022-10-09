8aea3c8e80659e0453a2d2f8bf15e3e9aa4aa161
golam mostafa <gmnayeem25@gmail.com>
2022-10-10 00:37:03 +0600

    display sidebar and modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Container, Row, Col} from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+import Sidebar from './components/sidebar';
+import MainContent  from './components/main-content'
+import POLLS from './components/data/polls'
+import shortid from 'shortid';
+
+class App extends Component {
+
+  state = {
+    polls: [],
+    selectedPoll: {},
+    searchTerm: ''
+  }
+
+  componentDidMount() {
+    this.setState({polls: POLLS})
+  }
+
+  addNewPoll = (poll) => {
+    poll.id = shortid.generate()
+    poll.created = new Date()
+    poll.totalVote = 0
+    poll.opinion = []
+
+    this.setState({
+      polls: this.state.polls.concat(poll)
+    })
+  }
+
+  updatePoll = (updatedPoll) => {
+    const {polls} = this.state
+    const poll = polls.filter(poll => poll.id === updatedPoll.id)
+
+    poll.title = updatedPoll.title
+    poll.description = updatedPoll.description
+    poll.options = updatedPoll.options
+
+    this.setState({polls})
+  }
+
+  deletePoll = (pollId) => {
+    const polls = this.state.polls.filter(poll => poll.id !== pollId)
+    this.setState({polls})
+  }
+
+  selectPoll = (pollId) => {
+    const poll = this.state.polls.find(poll => poll.id !== pollId)
+    this.setState({selectedPoll: poll})
+  }
+
+  handleSearch = (searchValue) => {
+
+  }
+  
+
+  render() {
+    return (
+      <Container className='my-5'>
+        <Row>
+          <Col md={4}>
+            <Sidebar 
+              polls={this.state.polls} 
+              selectPoll={this.selectPoll}
+              searchTerm={this.state.searchTerm}
+              handleSearch={this.handleSearch}
+            />
+          </Col>
+          <Col md={4}>
+            <MainContent />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { Input, Button, Modal, ModalHeader, ModalBody } from 'reactstrap'
+import PollList from './poll-list'
+
+export default class Sidebar extends Component {
+
+  state = {
+    openModal: false
+  }
+
+  toggleModal = () => {
+    this.setState({openModal: !this.state.openModal})
+  }
+
+
+  render() {
+    return (
+      <div style={{ background: '#efefef', padding: '10px' }}>
+        <div className='d-flex mb-5'>
+          <Input
+            type='search'
+            placeholder='Search'
+            value={this.props.searchTerm}
+            onChange={(e) => this.props.handleSearch(e.target.value)}
+          />
+          <Button
+            color='success'
+            style={{marginLeft: '10px'}}
+            onClick={this.toggleModal}
+          >
+            New
+          </Button>
+        </div>
+        <h3>List of Polls</h3>
+        <hr />
+        <PollList
+          polls={this.props.polls}
+          selectPoll={this.props.selectPoll}
+        />
+        <Modal 
+          isOpen={this.state.openModal}
+          toggle={this.toggleModal}
+          unmountOnClose={true}
+        >
+          <ModalHeader toggle={this.toggleModal}>
+            Creae A New Poll
+          </ModalHeader>
+          <ModalBody>
+            This is modal body
+          </ModalBody>
+        </Modal>
+      </div>
+    )
+  }
+}
